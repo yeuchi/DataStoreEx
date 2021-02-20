a69d96333ffd9875518cb7f7e339baf5f6286ccb
Chi Yeung <yeun0007@umn.edu>
2021-02-20 07:41:29 -0600

    initial shell with fragments.

---
+++ app/src/main/java/com/ctyeung/datastoreex/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.ctyeung.datastoreex
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.AttributeSet
+import android.view.View
+import android.widget.Button
+
+/*
+ * Jetpack DataStore
+ * 1. Preference datastore - key-value pair
+ * 2. Proto datastore - custom data type
+ */
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateView(name: String, context: Context, attrs: AttributeSet): View? {
+        val view = super.onCreateView(name, context, attrs)
+
+        return view
+    }
+}
---
+++ app/src/main/java/com/ctyeung/datastoreex/MainFragment.kt
@@ -0,0 +1,44 @@
+package com.ctyeung.datastoreex
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
+
+
+class MainFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val btn_pref = activity?.findViewById(R.id.btn_preference) as Button
+        btn_pref?.apply {
+            setOnClickListener{
+                findNavController().navigate(R.id.action_mainFragment_to_preferenceFragment)
+            }
+        }
+
+        val btn_proto = activity?.findViewById(R.id.btn_proto) as Button
+        btn_proto?.apply {
+            setOnClickListener{
+                findNavController().navigate(R.id.action_mainFragment_to_protoFragment)
+            }
+        }
+    }
+}
