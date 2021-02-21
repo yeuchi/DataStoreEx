7ceb25cf9de1f2e41e5f5eddea641360f8c7ebcc
Chi Yeung <yeun0007@umn.edu>
2021-02-20 19:03:14 -0600

    functional preference

--- app/src/main/java/com/ctyeung/datastoreex/ui/PreferenceFragment.kt
+++ app/src/main/java/com/ctyeung/datastoreex/ui/PreferenceFragment.kt
@@ -1,11 +1,14 @@
 package com.ctyeung.datastoreex.ui
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.EditText
+import android.widget.RadioButton
 import android.widget.RadioGroup
 import android.widget.TextView
 import androidx.lifecycle.Observer
@@ -16,6 +19,9 @@ class PreferenceFragment : Fragment() {
 
     private lateinit var model: PreferenceViewModel
     private lateinit var rdoGroup:RadioGroup
+    private lateinit var rdo1:RadioButton
+    private lateinit var rdo2:RadioButton
+    private lateinit var root:View
     private lateinit var editText: EditText
     private lateinit var txtString:TextView
     private lateinit var txtBoolean: TextView
@@ -30,18 +36,51 @@ class PreferenceFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val view = inflater.inflate(R.layout.fragment_preference, container, false)
-        rdoGroup = view.findViewById(R.id.rdo_group)
-        txtBoolean = view.findViewById(R.id.txt_boolean)
+        root = inflater.inflate(R.layout.fragment_preference, container, false)
 
-        editText = view.findViewById(R.id.edit_string)
-        txtString = view.findViewById(R.id.txt_string)
-        return view
+        root?.let {
+            rdoGroup = it.findViewById(R.id.rdo_group)
+            rdo1 = it.findViewById(R.id.rdo1)
+            rdo2 = root.findViewById(R.id.rdo2)
+            txtBoolean = root.findViewById(R.id.txt_boolean)
+
+            editText = root.findViewById(R.id.edit_string)
+            txtString = root.findViewById(R.id.txt_string)
+
+            initListeners()
+        }
+
+        return root
+    }
+
+    private fun initListeners() {
+        rdoGroup.setOnCheckedChangeListener(
+            RadioGroup.OnCheckedChangeListener { group, checkedId ->
+                var btn = root.findViewById(checkedId) as RadioButton
+                btn.isChecked = true
+                when(btn.id) {
+                    R.id.rdo1 -> model.setPrefBoolean(true)
+                    R.id.rdo2 -> model.setPrefBoolean(false)
+                }
+            })
+
+        editText.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                model.setPrefString(s.toString())
+            }
+        })
     }
 
     override fun onResume() {
         super.onResume()
         model.prefBoolean.observe(this, Observer(::onChangeBoolean) )
+        model.prefString.observe(this, Observer(::onChangeString) )
     }
 
     private fun onChangeBoolean(flag:Boolean) {
